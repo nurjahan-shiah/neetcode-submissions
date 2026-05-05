6b185cd2dd845523958d7be1811ed2e763dea2c0
Nurjahan Shiah <70992112+nurjahan-shiah@users.noreply.github.com>
2026-05-05 16:42:01 -0400

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,12 @@
+a, b, c = 2, 2, 0.5
+
+#result of divison is always a float in python. 
+#also if the other/any variable is float, output is float as well
+
+print(a+b+c)
+print(0-(a+b+c))
+print(a*b*c)
+print((a+b+c)/(a*b*c))
+
+
+
